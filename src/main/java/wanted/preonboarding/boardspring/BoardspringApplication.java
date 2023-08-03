21101e299244b81140f16f76f22646f1021659a1
nahyeon99 <nahyeonee99@gmail.com>
2023-08-03 13:45:58 +0900

    [Feat] initial commit

---
+++ src/main/java/wanted/preonboarding/boardspring/BoardspringApplication.java
@@ -0,0 +1,13 @@
+package wanted.preonboarding.boardspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wanted/preonboarding/boardspring/BoardspringApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.preonboarding.boardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
