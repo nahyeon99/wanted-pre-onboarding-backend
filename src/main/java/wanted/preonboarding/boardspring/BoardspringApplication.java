8891e6d7ace62854ce231a0e7d90f298565306ed
nahyeon99 <nahyeonee99@gmail.com>
2023-08-16 20:43:41 +0900

    [Chore] local, prod profile config 경로 추가

--- src/main/java/wanted/preonboarding/boardspring/BoardspringApplication.java
+++ src/main/java/wanted/preonboarding/boardspring/BoardspringApplication.java
@@ -1,15 +1,20 @@
 package wanted.preonboarding.boardspring;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class BoardspringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardspringApplication.class, args);
-	}
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml, " +
+			" /home/ec2-user/app/application-prod-db.yml";
 
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(BoardspringApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+	}
 }
