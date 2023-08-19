070fc195f60db841059ef19f4a490dbbdbd80ef6
nahyeon99 <nahyeonee99@gmail.com>
2023-08-19 13:54:07 +0900

    [Fix] profile config 오류

--- src/main/java/wanted/preonboarding/boardspring/BoardspringApplication.java
+++ src/main/java/wanted/preonboarding/boardspring/BoardspringApplication.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BoardspringApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml" + ",/home/ec2-user/app/application-prod.yml";
+			+ "classpath:application.yml"
+//			+ ",/Users/nahyeon/dev/externalfiles/application-local.yml";
+			+ ",/home/ec2-user/app/application-prod.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(BoardspringApplication.class)
